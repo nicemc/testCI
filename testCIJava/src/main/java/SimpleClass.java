3b272dd9bb38660154cbe371e84e4c2593e1b883
Christophe Nys <nicemc@gmail.com>
2015-09-01 09:36:37 +0200

    test commit

---
+++ testCIJava/src/main/java/SimpleClass.java
@@ -0,0 +1,14 @@
+/**
+ * Created by chrnys on 31/08/2015.
+ */
+public class SimpleClass {
+
+    public static void main(String[] args) {
+        System.out.println("Test CI");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+}
--- testCIJava/src/test/java/PipoTest.java
+++ testCIJava/src/test/java/PipoTest.java
@@ -11,6 +11,11 @@ public class PipoTest {
     @Test
     public void pipoTest1() {
         String s1 = "test";
-        assertThat(s1).isEqualTo("test2");
+        assertThat(s1).isEqualTo("test");
+    }
+
+    @Test
+    public void testAdd() {
+        assertThat(SimpleClass.add(2,3)).isEqualTo(5);
     }
 }
